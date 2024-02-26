760c0529e6b768b21bf1f6bfd98bcb2829046854
Anel-Vilca-78 <213378@ids.upchiapas.edu.mx>
2024-02-26 07:37:24 -0600

    un commit

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ window.addEventListener('scroll', ()=>{
     
     var scroll = window.scrollY
     if(scroll>10){
-    header.style.backgroundColor = '#121212'
+    header.style.backgroundColor = 'rgb(200, 230, 7)'
     }else{
-    header.style.backgroundColor = 'transparent'
+    header.style.backgroundColor = 'rgb(200, 230, 7)'
     }
 })
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let current = 0;
+    const images = document.querySelectorAll('.carousel-images img');
+    const totalImages = images.length;
+
+    // Función para actualizar el carrusel
+    function updateCarousel() {
+        images.forEach((img, index) => {
+            img.classList.remove('active');
+        });
+        images[current].classList.add('active');
+    }
+
+    // Inicializa el carrusel
+    updateCarousel();
+
+    // Cambia a la siguiente imagen cada 3 segundos
+    setInterval(() => {
+        current = (current + 1) % totalImages;
+        updateCarousel();
+    }, 3000);
+});
